91574beffcd1c2708b690dc0edb9a9d0e8136eaf
Lori Krell <jadehorn@gmail.com>
2021-07-10 13:10:13 -0500

    fixed order

--- src/components/timeline-conquest/data.js
+++ src/components/timeline-conquest/data.js
@@ -1,34 +1,84 @@
 const items = [
-  
     {
-      title: "38 L.C. / Jan 2018 Argus",
-      cardTitle: "Siege of Antorus",
-      cardSubtitle:
-        "That which you have stolen will be your undoing...",
+      title: "40 L.C. / April 2021",
+      cardTitle: "Tropic Sunder",
+      cardSubtitle: `Of course. I bring de word from Zul’Gurub. We tink it be time for new leadahs here in de Vale.`,
+      cardDetailedText: `Replaced`,
+        media: {
+          name: "tropic sunder",
+          source: {
+            url: "img/landing/tropic-logo.png"
+          },
+          type: "IMAGE"
+        }
+    },
+    {
+      title: "39 L.C. / May 2020",
+      cardTitle: "Visions of the Vale",
+      cardSubtitle: `“The accused has been found guilty of crimes against the Pandaren people, and treason against brothers and sisters-in-arms in the campaign against N’zoth.” The dreamer’s sleep was interrupted--`,
+      cardDetailedText: `Replaced`,
+        media: {
+          name: "visions vale",
+          source: {
+            url: "img/landing/vision-vale.png"
+          },
+          type: "IMAGE"
+        }
+    },
+    {
+      title: "39 L.C. / Sept 2020",
+      cardTitle: "In Death's Shadow",
+      cardSubtitle: `Forces of the Alliance and Horde descended upon the Forgotten Shore with the hopes they could put the fallen spirits to rest held in their hearts. But the rage and torment caused by the tearing of the sky made such a mission impossible to complete. `,
+      cardDetailedText: `Replaced`,
+        media: {
+          name: "death shadow",
+          source: {
+            url: "img/landing/death-shadow.png"
+          },
+          type: "IMAGE"
+        }
+    },
+    {
+      title: "39 L.C. / Feb 2020",
+      cardTitle: "Shadow in the Sands",
+      cardSubtitle: `We are....Ș̸̓A̷̍ͅT̴̜́H̴̢̍O̵̙̔G̴̬̋... That Which They Woke...YOU woke...`,
+      cardDetailedText: `Replaced`,
+        media: {
+          name: "shadow sands",
+          source: {
+            url: "img/landing/shadow-sands.png"
+          },
+          type: "IMAGE"
+        }
+    },
+    {
+      title: "38 L.C. / Oct 2019",
+      cardTitle: "Siege of Nazjatar",
+      cardSubtitle: `This was the closest the mind could accomplish to understanding the Laughter of a freed, reborn, awake One. He who was ancient before the World had cooled.`,
+      cardDetailedText: `Replaced`,
+        media: {
+          name: "siege of nazjatar",
+          source: {
+            url: "img/landing/siege-nazjatar.png"
+          },
+          type: "IMAGE"
+        }
+    },
+    {
+      title: "38 L.C. / Aug 2019",
+      cardTitle: "The Argent Trial",
+      cardSubtitle: `The Void aberration loomed above, growing fat off of the souls of the war-dead.`,
       cardDetailedText: `Replaced`,
       media: {
         name: "siege of antoras",
         source: {
-          url: "img/landing/siege-antorus.png"
+          url: "img/landing/argent-trial.png"
         },
         type: "IMAGE"
       }
     },
     {
-      title: "38 L.C. / March 2019 Zandalar",
-      cardTitle: "Conquest of Zandalar",
-      cardSubtitle: `No, I will not surrender. Because no matter what happens here today... Zandalar will stand long after your Alliance has crumbled to dust.`,
-      cardDetailedText: `Replaced`,
-      media: {
-          name: "zandalar",
-          source: {
-            url: "img/landing/conquest-zandalar.png"
-          },
-          type: "IMAGE"
-      }
-    },
-    {
-      title: "38 L.C. / July 2019 Vashj'ir",
+      title: "38 L.C. / July 2019",
       cardTitle: "Depths of Vashj'ir",
       cardSubtitle: `Carve through the landwalkers...sing your siren's call...heed the rising shadows...our time has come!`,
       cardDetailedText: `Replaced`,
@@ -42,82 +92,31 @@ const items = [
       }
     },
     {
-      title: "38 L.C. / August 2019 Wetlands",
-      cardTitle: "The Argent Trial",
-      cardSubtitle: `The Void aberration loomed above, growing fat off of the souls of the war-dead.`,
+      title: "38 L.C. / March 2019",
+      cardTitle: "Conquest of Zandalar",
+      cardSubtitle: `No, I will not surrender. Because no matter what happens here today... Zandalar will stand long after your Alliance has crumbled to dust.`,
+      cardDetailedText: `Replaced`,
+      media: {
+          name: "zandalar",
+          source: {
+            url: "img/landing/conquest-zandalar.png"
+          },
+          type: "IMAGE"
+      }
+    },
+    {
+      title: "38 L.C. / Jan 2018",
+      cardTitle: "Siege of Antorus",
+      cardSubtitle:
+        "That which you have stolen will be your undoing...",
       cardDetailedText: `Replaced`,
       media: {
         name: "siege of antoras",
         source: {
-          url: "img/landing/argent-trial.png"
+          url: "img/landing/siege-antorus.png"
         },
         type: "IMAGE"
       }
-    },
-    {
-      title: "38 L.C. / Oct 2019 Nazjatar",
-      cardTitle: "Siege of Nazjatar",
-      cardSubtitle: `This was the closest the mind could accomplish to understanding the Laughter of a freed, reborn, awake One. He who was ancient before the World had cooled.`,
-      cardDetailedText: `Replaced`,
-        media: {
-          name: "siege of nazjatar",
-          source: {
-            url: "img/landing/siege-nazjatar.png"
-          },
-          type: "IMAGE"
-        }
-    },
-    {
-      title: "39 L.C. / Feb 2020 Uldum",
-      cardTitle: "Shadow in the Sands",
-      cardSubtitle: `We are....Ș̸̓A̷̍ͅT̴̜́H̴̢̍O̵̙̔G̴̬̋... That Which They Woke...YOU woke...`,
-      cardDetailedText: `Replaced`,
-        media: {
-          name: "shadow sands",
-          source: {
-            url: "img/landing/shadow-sands.png"
-          },
-          type: "IMAGE"
-        }
-    },
-    {
-      title: "39 L.C. / May 2020 Panadria",
-      cardTitle: "Visions of the Vale",
-      cardSubtitle: `“The accused has been found guilty of crimes against the Pandaren people, and treason against brothers and sisters-in-arms in the campaign against N’zoth.” The dreamer’s sleep was interrupted--`,
-      cardDetailedText: `Replaced`,
-        media: {
-          name: "visions vale",
-          source: {
-            url: "img/landing/vision-vale.png"
-          },
-          type: "IMAGE"
-        }
-    },
-    {
-      title: "39 L.C. / Sept 2020 Northrend",
-      cardTitle: "In Death's Shadow",
-      cardSubtitle: `Forces of the Alliance and Horde descended upon the Forgotten Shore with the hopes they could put the fallen spirits to rest held in their hearts. But the rage and torment caused by the tearing of the sky made such a mission impossible to complete. `,
-      cardDetailedText: `Replaced`,
-        media: {
-          name: "death shadow",
-          source: {
-            url: "img/landing/death-shadow.png"
-          },
-          type: "IMAGE"
-        }
-    },
-    {
-      title: "40 L.C. / April 2021 Stranglethorn Vale",
-      cardTitle: "Tropic Sunder",
-      cardSubtitle: `Of course. I bring de word from Zul’Gurub. We tink it be time for new leadahs here in de Vale.`,
-      cardDetailedText: `Replaced`,
-        media: {
-          name: "tropic sunder",
-          source: {
-            url: "img/landing/tropic-logo.png"
-          },
-          type: "IMAGE"
-        }
     },
   ];
   
