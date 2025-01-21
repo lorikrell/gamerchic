5420262470181e4fc5486610fd57cbd4e23eeac3
Lori Krell <jadehorn@gmail.com>
2025-01-20 21:26:59 -0600

    bluesky

--- src/pages/about.js
+++ src/pages/about.js
@@ -37,6 +37,7 @@ function SoncGallery() {
         
         <p><a href="https://ko-fi.com/sonceriart" target="_blank"><img src="img/kofi.png"/> Sonceri on Ko-fi</a><br/>
         <a href="https://lorikrell.myportfolio.com/work" target="_blank"><img src="img/portfolio.png"/> Full Online Portfolio</a><br/>
+        <a href="https://bsky.app/profile/sonceri.bsky.social" target="_blank"><img src="img/bluesky.png"/> Sonceri on Bluesky</a><br/>
         <a href="https://sonceri-mg.tumblr.com/" target="_blank"><img src="img/tumblr.png"/> Sonceri on Tumblr</a><br/>
         <a href="https://twitter.com/lorioliodraws" target="_blank"><img src="img/twitter.png"/> Lorioliodraws on Twitter/X</a><br/>
         <img src="img/discord.png"/> loriolio on Discord</p>
